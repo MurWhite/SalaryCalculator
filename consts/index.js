0ba0a1f39b2b7b46fa3865d27ecd902838055ac6
Mengyuan Xiong <xiongmengyuan@bytedance.com>
2023-01-11 22:48:25 +0800

    feat: init project

---
+++ consts/index.js
@@ -0,0 +1,127 @@
+export const CHILDREN_DEDUCT = [{
+	label: "请选择",
+	value: 0
+}, {
+	label: "不符合，扣除0元",
+	value: 0
+}, {
+	label: "一个孩子，夫妻各扣除500元",
+	value: 500
+}, {
+	label: "一个孩子，仅有一方扣除1000元",
+	value: 1000
+}, {
+	label: "两个孩子，夫妻各扣除1000元",
+	value: 1000
+}, {
+	label: "两个孩子，仅有一方扣除2000元",
+	value: 2000
+}]
+
+
+export const EDU_DEDUCT = [{
+	label: "请选择",
+	value: 0
+}, {
+	label: "不符合，扣除0元",
+	value: 0
+}, {
+	label: "接受学历教育中，扣除400",
+	value: 400
+}]
+
+export const PARENT_DEDUCT = [{
+	label: "指定分摊",
+	value: 0
+}, {
+	label: "约定分摊",
+	value: 1
+}]
+
+export const PARENT_DEDUCT_OPTIONS = [{
+		label: "请选择",
+		value: 0
+	},
+	{
+		label: "不符合，扣除0元",
+		value: 0
+	}, {
+		label: "十兄弟姐妹分摊，扣除200元",
+		value: 200
+	}, {
+		label: "九兄弟姐妹分摊，扣除222.2元",
+		value: 222.2
+	}, {
+		label: "八兄弟姐妹分摊，扣除250元",
+		value: 250
+	}, {
+		label: "七兄弟姐妹分摊，扣除285.7元",
+		value: 285.7
+	}, {
+		label: "六兄弟姐妹分摊，扣除333.3元",
+		value: 333.3
+	}, {
+		label: "五兄弟姐妹分摊，扣除400元",
+		value: 400
+	}, {
+		label: "四兄弟姐妹分摊，扣除500元",
+		value: 500
+	}, {
+		label: "三兄弟姐妹分摊，扣除666.7元",
+		value: 666.7
+	}, {
+		label: "两兄弟姐妹分摊，扣除1000元",
+		value: 1000
+	},
+	{
+		label: "独生子女，扣除2000元",
+		value: 2000
+	}
+]
+
+
+export const BUY_HOUSE_DEDUCT = [{
+	label: '请选择',
+	value: 0
+}, {
+	label: '不符合，扣除0元',
+	value: 0
+}, {
+	label: '夫妻各扣除500元',
+	value: 500
+}, {
+	label: '仅由一方扣除1000元',
+	value: 1000
+}]
+
+
+export const RETENT_HOUSE_DEDUCT = [{
+	label: '请选择',
+	value: 0
+}, {
+	label: '不符合，扣除0元',
+	value: 0
+}, {
+	label: '所在城市户籍人口少于100万，扣除800元',
+	value: 800
+}, {
+	label: '所在城市户籍人口超过100万，扣除1100元',
+	value: 1100
+}, {
+	label: '直辖市、省会城市、计划单列市等，扣除1500元',
+	value: 1500
+}]
+
+export const BABY_DEDUCT = [{
+	label: '请选择',
+	value: 0
+}, {
+	label: '不符合，扣除0元',
+	value: 0
+}, {
+	label: '一方扣除1000元',
+	value: 1000
+}, {
+	label: '夫妻各扣除500元',
+	value: 500
+}]
---
+++ pages/home/index.vue
@@ -0,0 +1,212 @@
+<template>
+	<view class="salary-content">
+		<view class="salary-form">
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">所属地区</view>
+				<view>
+					<picker :value="cityIndex" :range="cityList" @change="bindPickerChange">
+						<view>{{cityList[cityIndex]}}</view>
+					</picker>
+				</view>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">税前基本工资</view>
+				<view>
+					<input v-model="salary" focus placeholder="请输入税前工资" />
+				</view>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">是否缴纳社保</view>
+				<radio-group @change="e =>onRadioChange('paySocial', e)" class="salary-form-item_pay_social">
+					<label class="mr-8 flex-items-center">
+						<view>
+							<radio value="1" :checked="paySocial === 1" />
+						</view>
+						<view>是</view>
+					</label>
+					<label class="flex-items-center">
+						<view>
+							<radio value="0" :checked="paySocial === 0" />
+						</view>
+						<view>否</view>
+					</label>
+				</radio-group>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">社保缴纳基数</view>
+				<view class="salary-form-item_content">
+					<picker :value="socialBaseIndex" :range="socialBaseList" @change="bindPickerChange">
+						<view>{{socialBaseList[socialBaseIndex]}}</view>
+					</picker>
+				</view>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">社保基数</view>
+				<view class="salary-form-item_content">
+					<input v-model="salary" focus placeholder="请输入自定义社保缴纳基数" />
+				</view>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">公积金缴纳基数</view>
+				<view class="salary-form-item_content">
+					<picker :value="fundIndex" :range="socialBaseList" @change="bindPickerChange">
+						<view>{{socialBaseList[fundIndex]}}</view>
+					</picker>
+				</view>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">公积金基数</view>
+				<view class="salary-form-item_content">
+					<input v-model="fund" focus placeholder="请输入自定义公积金基数" />
+				</view>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">公积金缴纳比例</view>
+				<view class="salary-form-item_content">
+					<input v-model="fundRatio" focus placeholder="请输入自定义公积金基数" />
+					<view>%</view>
+				</view>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">包含专项扣除</view>
+				<radio-group @change="e => onRadioChange('hasDeduct',e)" class="salary-form-item_pay_social">
+					<label class="mr-8 flex-items-center">
+						<view>
+							<radio value="1" :checked="hasDeduct === 1" />
+						</view>
+						<view>包含</view>
+					</label>
+					<label class="flex-items-center">
+						<view>
+							<radio value="0" :checked="hasDeduct === 0" />
+						</view>
+						<view>不包含</view>
+					</label>
+				</radio-group>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">子女教育扣除</view>
+				<view class="salary-form-item_content">
+					<picker :value="childrenDeductIndex" range-key="label" :range="CHILDREN_DEDUCT"
+						@change="bindPickerChange">
+						<view>{{CHILDREN_DEDUCT[childrenDeductIndex].label}}</view>
+					</picker>
+				</view>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">继续教育扣除</view>
+				<view class="salary-form-item_content">
+					<picker :value="eduDeductIndex" range-key="label" :range="EDU_DEDUCT" @change="bindPickerChange">
+						<view>{{EDU_DEDUCT[eduDeductIndex].label}}</view>
+					</picker>
+				</view>
+			</view>
+			<view class="salary-form-item">
+				<view class="salary-form-item_label">赡养老人扣除</view>
+				<view class="salary-form-item_content">
+					<picker :value="eduDeductIndex" range-key="label" :range="EDU_DEDUCT" @change="bindPickerChange">
+						<view>{{EDU_DEDUCT[eduDeductIndex].label}}</view>
+					</picker>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import '@/styles/index.scss'
+	import {
+		CHILDREN_DEDUCT,
+		EDU_DEDUCT,
+		PARENT_DEDUCT,
+		PARENT_DEDUCT_OPTIONS,
+		BUY_HOUSE_DEDUCT,
+		RETENT_HOUSE_DEDUCT,
+		BABY_DEDUCT
+	} from '@/consts/index'
+
+	export default {
+		data() {
+			return {
+				title: 'Hello2',
+				cityList: ['中国', '美国', '巴西', '日本'],
+				socialBaseList: ['按照工资', '按照最低标准', '自定义'],
+				cityIndex: 0,
+				socialBaseIndex: 0,
+				fundIndex: 0, // 公积金缴纳选项
+				salary: '', // 社保缴纳值
+				fund: '', // 公积金缴纳值
+				fundRatio: 7, // 公积金缴纳比例
+				paySocial: 1, // 是否缴纳社保
+				hasDeduct: 1, // 是否有专项扣除
+				childrenDeductIndex: 0,
+				eduDeductIndex: 0,
+				parentDeductIndex: 0,
+				parentDeductSelectIndex: 0,
+				buyHouseIndex: 0,
+				retentHouseIndex: 0,
+				babyIndex: 0,
+				CHILDREN_DEDUCT,
+				EDU_DEDUCT,
+				PARENT_DEDUCT,
+				PARENT_DEDUCT_OPTIONS,
+				BUY_HOUSE_DEDUCT,
+				RETENT_HOUSE_DEDUCT,
+				BABY_DEDUCT
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			bindPickerChange(e) {
+				console.log('picker发送选择改变，携带值为', e.detail.value)
+				this.cityIndex = e.detail.value
+			},
+			onRadioChange(key, e) {
+				this[key] = e.detail.value
+				console.log('是否缴纳社保', this.paySocial, typeof this.paySocial)
+			}
+		},
+		components: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.salary-content {
+		.salary-form {
+			padding: 10px;
+
+			&-item {
+				padding: 12px 0;
+				display: flex;
+				align-items: center;
+				border-bottom: 1px solid #E3E5E7;
+
+				&_label {
+					flex-shrink: 0;
+					width: 130px;
+					color: $uni-text-color-grey;
+					font-size: 16px;
+					line-height: 22px;
+					margin: 0 5px 0 0;
+				}
+
+				&_content {
+					flex: 1;
+					display: flex;
+
+					input {
+						flex: 1;
+					}
+				}
+
+				&_pay_social {
+					display: flex;
+				}
+			}
+		}
+	}
+</style>
